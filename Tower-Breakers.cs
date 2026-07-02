46de2d232f8ff6224c2120ba476750bdc076eefb
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-07-03 02:53:41 +0900

    Update Tower-Breakers.cs

--- Tower-Breakers.cs
+++ Tower-Breakers.cs
@@ -1,1 +1,32 @@
-Tower-Breakers
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m)
+    {
+        return (m == 1 || n % 2 == 0) ? 2 : 1;
+    }
+
+}
